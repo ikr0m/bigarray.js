b713aebacce13b043dc50c52c480b4726d2bea44
Ikrom <ikrom@tocobox.com>
2015-06-01 17:25:21 +0500

    dist folder

---
+++ dist/bigarray.js
@@ -0,0 +1,20 @@
+
+/*
+    bigarray.js 0.0.1
+    2015-05-31
+
+    By Ikrom, https://github.com/ikr0m
+    License: MIT
+ */
+
+(function() {
+  var BigArray;
+
+  BigArray = (function() {
+    function BigArray(array, name) {}
+
+    return BigArray;
+
+  })();
+
+}).call(this);
